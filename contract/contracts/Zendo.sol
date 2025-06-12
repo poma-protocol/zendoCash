3b0ba7f382188c64fe9d2d64e6a42d917db17603
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2025-06-12 13:33:01 +0300

    feat: final contract

--- contract/contracts/Zendo.sol
+++ contract/contracts/Zendo.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+
 contract Zendo {
     struct Deal {
         uint32 id;
@@ -15,11 +14,21 @@ contract Zendo {
         uint16 numberOfDays;
         uint256 startDate;
         uint256 endDate;
-        address[] participants;
+        uint32 currentParticipantCount;
         bool isActive;
         address tokenAddress;
     }
+
+    struct Participant {
+        address participantAddress;
+        uint256 rewardsClaimed;
+        bool isRewarded;
+        bool isRegistered;
+    }
+
     mapping(uint32 => Deal) public deals;
+    mapping(uint32 => mapping(address => Participant)) public dealParticipants;
+
     event DealCreated(
         uint32 id,
         string dealName,
@@ -57,6 +66,8 @@ contract Zendo {
         newDeal.endDate = _endDate;
         newDeal.isActive = false;
         newDeal.tokenAddress = _tokenAddress;
+        newDeal.currentParticipantCount = 0;
+
         emit DealCreated(
             _id,
             _dealName,
@@ -69,21 +80,62 @@ contract Zendo {
             _endDate,
             _tokenAddress
         );
+
         return _id;
     }
 
-    function activateDeal(uint32 _id, address tokenAddress) external{
+    function activateDeal(uint32 _id, address tokenAddress) external {
         Deal storage deal = deals[_id];
         require(deal.id == _id, "Deal does not exist");
         require(!deal.isActive, "Deal is already active");
         require(deal.endDate >= block.timestamp, "Deal has ended");
-        
+
         IERC20 token = IERC20(tokenAddress);
         uint256 amount = deal.rewards;
         bool success = token.transferFrom(msg.sender, address(this), amount);
         require(success, "Transfer failed");
         deal.isActive = true;
-
     }
 
+    function addParticipant(uint32 _id, address participant) external {
+        Deal storage deal = deals[_id];
+        require(deal.id == _id, "Deal does not exist");
+        require(deal.isActive, "Deal is not active");
+        require(deal.currentParticipantCount < deal.maxParticipants, "Max participants reached");
+        require(
+            block.timestamp >= deal.startDate && block.timestamp <= deal.endDate,
+            "Deal is not active"
+        );
+
+        Participant storage p = dealParticipants[_id][participant];
+        require(!p.isRegistered, "Participant already registered");
+
+        dealParticipants[_id][participant] = Participant({
+            participantAddress: participant,
+            rewardsClaimed: 0,
+            isRewarded: false,
+            isRegistered: true
+        });
+
+        deal.currentParticipantCount += 1;
+    }
+
+    function rewardUser(uint32 _id, address participant) external {
+        Deal storage deal = deals[_id];
+        require(deal.id == _id, "Deal does not exist");
+        require(deal.isActive, "Deal is not active");
+        require(deal.endDate > block.timestamp, "Deal has ended");
+
+        Participant storage p = dealParticipants[_id][participant];
+        require(p.isRegistered, "Participant not found");
+        require(!p.isRewarded, "User already rewarded");
+
+        uint256 amount = deal.rewards / deal.maxParticipants;
+        IERC20 token = IERC20(deal.tokenAddress);
+        bool success = token.transfer(participant, amount);
+        require(success, "Transfer failed");
+
+        p.rewardsClaimed = amount;
+        p.isRewarded = true;
+    }
 }
