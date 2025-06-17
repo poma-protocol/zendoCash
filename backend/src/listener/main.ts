df6a5ee944abdd27150c8103eca0008784af6dc9
Roman Njoroge <roman.njoroge@njuguna.com>
2025-06-17 13:17:22 +0300

    feat: Cron job for running main every hour:

--- backend/src/listener/main.ts
+++ backend/src/listener/main.ts
@@ -1,17 +1,30 @@
 import dealsController from "../controller/deals";
+import dealModel from "../model/deals";
+import smartContract from "../smartContract";
+import processMainDeal from "./processor";
+import {CronJob} from "cron";
 
 export default async function main() {
     try {
+        console.log("RUNNING DEALS PROCESSOR");
         // Get all deals and their participants
         const deals = await dealsController.mainFunctionDeals();
+        console.log("Deals", deals);
 
         // For each deal
         for (const deal of deals) {
-            
+            console.log("PROCESSING DEAL", deal);
+            await processMainDeal(deal, dealsController, smartContract, dealModel);
         }
     } catch (err) {
         console.error("Error occured in the listener", err);
     }
 }
 
-main();
+const job = new CronJob(
+    '0 0 * * * *',
+    main,
+    null,
+    true,
+    'system'
+);
